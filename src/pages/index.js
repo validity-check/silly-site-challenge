9f48dde95b08d6c92b68ad5980976d63539134a8
validity-check <mzaiddar77@gmail.com>
2020-11-22 14:13:59 +0000

    Did more stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import global from "../../static/styles/global.css"
 
 import Brand from "../components/brand"
 import Header from "../components/header"
+// import { Link } from "gatsby"
 
 export default function Home() {
   return (
@@ -15,9 +16,33 @@ export default function Home() {
       <section className="brand">
         <div className="brand">
           <h1 className="brand center shake">The Silly Site</h1>
-          <Brand className="center shake" width="100%" height="500px" />
+          <Brand
+            className="center img brand shake flip"
+            width="100%"
+            height="500px"
+          />
         </div>
       </section>
+      <section className="gatsby-logo">
+        <a
+          href="https://www.gatsbyjs.com"
+          target="_blank"
+          className="gatsby-logo-desc"
+          rel="noreferrer"
+        >
+          Built with GatsbyJS, master of Javascript frameworks.
+        </a>
+      </section>
+      <br />
+      <p style={{ textAlign: "center" }}>
+        Scroll down - I have a nice button for you!
+      </p>
+      <br />
+      <br />
+      <br />
+      <button className="glow-on-hover" type="button">
+        Click this button for something fun to happen!
+      </button>
     </div>
   )
 }
