f4d2f7e6601e88e3e539ca53980f3f328f2b68d1
validity-check <mzaiddar77@gmail.com>
2020-11-22 11:59:58 +0000

    Did stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
 import React from "react"
 
+import index from "../../static/styles/pages/index.css"
+import global from "../../static/styles/global.css"
+
+import Brand from "../components/brand"
+import Header from "../components/header"
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="index">
+      <style src={global} />
+      <style src={index} />
+      <Header />
+      <section className="brand">
+        <div className="brand">
+          <h1 className="brand center shake">The Silly Site</h1>
+          <Brand className="center shake" width="100%" height="500px" />
+        </div>
+      </section>
+    </div>
+  )
 }
